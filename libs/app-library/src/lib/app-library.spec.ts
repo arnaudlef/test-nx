1c6d5feaca7e99c91b6597fb383c4777b07ba62d
Arnaud <arn.lefrancois@hotmail.fr>
2021-10-14 12:30:42 +0200

    add library

---
+++ libs/app-library/src/lib/app-library.spec.ts
@@ -0,0 +1,7 @@
+import { appLibrary } from './app-library';
+
+describe('appLibrary', () => {
+  it('should work', () => {
+    expect(appLibrary()).toEqual('app-library');
+  });
+});
---
+++ libs/app-library/src/lib/app-library.ts
@@ -0,0 +1,3 @@
+export function appLibrary(): string {
+  return 'app-library';
+}
